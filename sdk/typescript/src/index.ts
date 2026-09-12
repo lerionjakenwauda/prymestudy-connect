899e01952009f27080578ecfaee52404d85edb48
Joseph Ebubechukuwu Nwauda <81398503+joseph724-Fk@users.noreply.github.com>
2026-09-12 21:40:05 +0100

    feat(node): export connect webhook verifier

--- sdk/typescript/src/index.ts
+++ sdk/typescript/src/index.ts
@@ -4,4 +4,11 @@ export {
   type ConnectAlgorithm,
   type ConnectConfig,
   type LaunchResponse,
+  type UpsertResponse,
 } from "./client.js";
+
+export {
+  verifyWebhook,
+  WebhookVerificationError,
+  type WebhookVerificationOptions,
+} from "./webhook.js";
